7726cc3cf5cd4f0b984197797f3fdc9c89fee813
Shakil53 <shakilahmed.dcc@gmail.com>
2024-10-14 11:44:32 +0600

    In Diarrhea data loaded

---
+++ src/pages/Home/medicineAll/Diarrhea.jsx
@@ -0,0 +1,30 @@
+
+import { useQuery } from '@tanstack/react-query';
+import DiarrheaComponent from './categoryComponent/DiarrheaComponent';
+import { Key } from 'react';
+
+const Diarrhea = () => {
+    const getDiarrheaItem = async () => {
+        return await fetch('/medicine.json')
+            .then(res => res.json());
+    }
+
+
+    const { data, isLoading } = useQuery({
+        queryKey: ["diarrheaItem"],
+        queryFn: getDiarrheaItem,
+    })
+
+    if (isLoading) {
+        return <p>Loading...</p>
+    }
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 p-16 gap-4">
+            {
+                data?.map((item) => <DiarrheaComponent key={item.id} item={item}></DiarrheaComponent>)
+            }
+        </div>
+    );
+};
+
+export default Diarrhea;
