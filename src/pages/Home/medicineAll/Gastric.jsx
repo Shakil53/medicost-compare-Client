478b6d0171a2c55d423272a0c46795614ed1f940
Shakil53 <shakilahmed.dcc@gmail.com>
2024-10-14 11:54:23 +0600

    gastric data loaded

---
+++ src/pages/Home/medicineAll/Gastric.jsx
@@ -0,0 +1,30 @@
+
+import { useQuery } from "@tanstack/react-query";
+import GasticComponent from './categoryComponent/GasticComponent';
+import { Key } from "react";
+
+const Gastric = () => {
+
+    const getGasticItem = async () => {
+        return await fetch('/medicine.json').then(res => res.json());
+    }
+
+    const {data,isLoading} = useQuery({
+        queryKey: ["gasticItem"],
+        queryFn: getGasticItem,
+    })
+
+    if (isLoading) {
+        return <p>Loading..</p>
+    }
+
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 p-16 gap-4">
+            {
+                data?.map((item)=> <GasticComponent key={item.id} item={item}></GasticComponent>)
+            }
+        </div>
+    );
+};
+
+export default Gastric;
