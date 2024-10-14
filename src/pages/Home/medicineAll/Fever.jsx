3591c908a70ea06fc9d21455da64290ae11d25c5
Shakil53 <shakilahmed.dcc@gmail.com>
2024-10-14 11:53:45 +0600

    fever  data loaded

---
+++ src/pages/Home/medicineAll/Fever.jsx
@@ -0,0 +1,31 @@
+import { useQuery } from '@tanstack/react-query';
+
+import FeverComponent from './categoryComponent/FeverComponent';
+import { Key } from 'react';
+
+const Fever = () => {
+
+    const getFever = async() => {
+        return await fetch('/medicine.json').then(res => res.json());
+    }
+
+
+    const {data, isLoading} = useQuery({
+        queryKey: ["fever"],
+        queryFn: getFever,
+
+    })
+
+    if (isLoading) {
+        return <p>Loading..</p>
+    }
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 p-16 gap-4">
+            {
+                data?.map((item) => <FeverComponent key={item.id} item={item}></FeverComponent>)
+          }
+        </div>
+    );
+};
+
+export default Fever;
