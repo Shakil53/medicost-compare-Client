8b91233b4b04dd856a2e308a54beafabd90d63e3
Shakil53 <shakilahmed.dcc@gmail.com>
2024-10-14 12:07:40 +0600

    OralfungalInfaction compo  data loaded

---
+++ src/pages/Home/medicineAll/OralFungalInfection.jsx
@@ -0,0 +1,30 @@
+import { useQuery } from '@tanstack/react-query';
+import OralFungalInfectionComponent from './categoryComponent/OralFungalInfectionComponent';
+import { Key } from 'react';
+
+const OralFungalInfection = () => {
+    const getOralFungalInfection = async() => {
+        return await fetch('/medicine.json').then(res => res.json());
+    }
+
+
+    const {data, isLoading} = useQuery({
+        queryKey: ["oralFungalInfection"],
+        queryFn: getOralFungalInfection,
+
+    })
+
+    if (isLoading) {
+        return <p>Loading..</p>
+    }
+
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 p-16 gap-4">
+            {
+                data?.map((item)=> <OralFungalInfectionComponent key={item.id} item={item}></OralFungalInfectionComponent>)
+            }
+        </div>
+    );
+};
+
+export default OralFungalInfection;
