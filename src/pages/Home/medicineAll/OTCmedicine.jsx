bfaa4ac97a318faeb0fc2a551d49248ab915acfb
Shakil53 <shakilahmed.dcc@gmail.com>
2024-10-14 12:08:16 +0600

    OTC medicine compo  data loaded

---
+++ src/pages/Home/medicineAll/OTCmedicine.jsx
@@ -0,0 +1,39 @@
+import { useQuery } from "@tanstack/react-query";
+import OTCmedicineComponent from "./categoryComponent/OTCmedicineComponent";
+import { Key } from "react";
+
+
+const OTCmedicine = () => {
+
+    const getOtcMedicine = async () => {
+        return await fetch('/medicine.json').then(res => res.json());
+    }
+
+
+ 
+
+    const {data, isLoading} = useQuery({
+        queryKey: ["OTCmedicine"],
+        queryFn: getOtcMedicine,
+
+    })
+
+    //loading state
+    if (isLoading) {
+        return <p>Loading..</p>
+    }
+
+
+
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 p-16 gap-4">
+            
+                {
+                    data.map((item) => <OTCmedicineComponent key={item.id} item={item}></OTCmedicineComponent>)
+                }
+            
+        </div>
+    );
+};
+
+export default OTCmedicine;
