1865f2dab172a93897b40f185863fcc052fae5e9
Shakil53 <shakilahmed.dcc@gmail.com>
2024-10-14 11:56:57 +0600

    gynecological data loaded

---
+++ src/pages/Home/medicineAll/Gynecological.jsx
@@ -0,0 +1,31 @@
+import { useQuery } from '@tanstack/react-query';
+
+import GynecologicalComponent from './categoryComponent/GynecologicalComponent';
+import { Key } from 'react';
+
+const Gynecological = () => {
+    const getGynecological = async() => {
+        return await fetch('/medicine.json').then(res => res.json());
+    }
+
+
+    const {data, isLoading} = useQuery({
+        queryKey: ["gynecological"],
+        queryFn: getGynecological,
+
+    })
+
+    if (isLoading) {
+        return <p>Loading..</p>
+    }
+
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 p-16 gap-4">
+            {
+                data?.map((item) => <GynecologicalComponent key={item.id} item={item}></GynecologicalComponent>)
+            }
+        </div>
+    );
+};
+
+export default Gynecological;
