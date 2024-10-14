a47c326c2ec65cea13aa39a2d871ec43be581b36
Shakil53 <shakilahmed.dcc@gmail.com>
2024-10-14 12:09:25 +0600

    Ulcer data loaded

---
+++ src/pages/Home/medicineAll/Ulcer.jsx
@@ -0,0 +1,30 @@
+import { useQuery } from '@tanstack/react-query';
+import UlcerComponent from './categoryComponent/UlcerComponent';
+import { Key } from 'react';
+
+const Ulcer = () => {
+    const getUlcer = async() => {
+        return await fetch('/medicine.json').then(res => res.json());
+    }
+
+
+    const {data, isLoading} = useQuery({
+        queryKey: ["ulcerItem"],
+        queryFn: getUlcer,
+
+    })
+
+    if (isLoading) {
+        return <p>Loading..</p>
+    }
+
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 p-16 gap-4">
+            {
+                data?.map((item)=> <UlcerComponent key={item.id} item={item}></UlcerComponent>)
+            }
+        </div>
+    );
+};
+
+export default Ulcer;
