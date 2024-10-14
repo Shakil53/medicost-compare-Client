64e01b13c801f2ccc13d353ae3615b484fb5cd56
Shakil53 <shakilahmed.dcc@gmail.com>
2024-10-14 12:00:11 +0600

    nasal data loaded

---
+++ src/pages/Home/medicineAll/Nasal.jsx
@@ -0,0 +1,32 @@
+import { useQuery } from '@tanstack/react-query';
+
+import NasalComponent from './categoryComponent/NasalComponent';
+import { Key } from 'react';
+
+const Nasal = () => {
+
+    const getNasalItem = async () => {
+        return await fetch('/medicine.json').then(res => res.json());
+    } 
+
+
+    const {data, isLoading} = useQuery({
+        queryKey: ["nasalItem"],
+        queryFn: getNasalItem
+    })
+
+    if (isLoading) {
+        return <p>Loading..</p>
+    }
+
+
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 p-16 gap-4">
+            {
+                data.map((item) => <NasalComponent item={item} key={item.id}></NasalComponent>)
+            }
+        </div>
+    );
+};
+
+export default Nasal;
